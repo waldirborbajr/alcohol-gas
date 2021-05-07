8861a6c1fd15e15f8ddeabcc983a2209b70dd1fc
Waldir Borba Junior <wborbajr@gmail.com>
2021-05-07 11:39:20 -0300

    version updated

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Alcohol or Gas',
+      title: 'Alcohol (Ethanol) or Gas',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
